bc339a267c01b13e9f105fa7fb3cc74207fa2d5c
NatalliaKri <55781208+NatalliaKri@users.noreply.github.com>
2021-03-10 20:25:09 +0300

    init: implement 3 functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (a) 
+{
+    if (a === undefined || a.length === 0) return 0;
+    let minimum = a[0];
+    for (let i = 1; i < a.length; i++)
+    {
+        if (minimum > a[i]) minimum = a[i];
+        if (typeof a[i] !== "number") return 0;
+    }
+     return minimum;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (a) 
+{
+    if (a === undefined || a.length === 0) return 0;
+    let maximum = a[0];
+    for (let i = 1; i < a.length; i++)
+    {
+        if (maximum < a[i]) maximum = a[i];
+        if (typeof a[i] !== "number") return 0;
+    }
+    return maximum;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (a) 
+{
+    if (a === undefined || a.length === 0) return 0;
+    let sum = 0;
+    for (let i = 0; i < a.length; i++)
+    { 
+        sum += a[i];
+    }
+    if (typeof sum !== "number") return 0;
+    else return sum / a.length;
 }
